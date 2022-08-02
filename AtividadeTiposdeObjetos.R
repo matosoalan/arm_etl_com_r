c8ec6daba7325598f9d8494d110bfefe8b6ef7a5
matosoalan <110362847+matosoalan@users.noreply.github.com>
2022-08-02 18:01:49 -0300

    Atividade Tipos de Objetos

---
+++ AtividadeTiposdeObjetos.R
@@ -0,0 +1,15 @@
+timesFutebol<-c("sport","nautico","santa cruz","corinthians","flamengo")
+
+fundaçãoTimes <-c("1905","1901","1914","1910","1895")
+
+idadeTimes <-c ("117","121","108","112","127")
+
+titulosTimes <-c ("42","21","29","28","35")
+
+listaTimes <-c (data.frame(
+  times=timesFutebol,
+  fundação = fundaçãoTimes,
+  idade=idadeTimes,
+  titulos=titulosTimes
+))
+View(listaTimes)
