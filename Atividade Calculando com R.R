5713685c01908737402f6fa6d3c3fe52145709c6
matosoalan <110362847+matosoalan@users.noreply.github.com>
2022-08-29 21:43:34 -0300

    Update Atividade Calculando com R.R

--- Atividade Calculando com R.R
+++ Atividade Calculando com R.R
@@ -1,11 +1,11 @@
 distribuicaoBinomial<-rnbinom(200,mu=2,size=5)
 hist(distribuicaoBinomial)
 
-poison<-rpois(200,2)
+poisson<-rpois(200,2)
 hist(poison)
 
 #centralização da base de dados
 
-poisonCentralizado<- poison-mean(poison)
+poissonCentralizado<- poisson-mean(poisson)
 
-hist(poisonCentralizado)
+hist(poissonCentralizado)
