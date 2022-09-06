b1565c65944cbcca0592e0e3394e220124bde367
matosoalan <110362847+matosoalan@users.noreply.github.com>
2022-09-06 15:27:35 -0300

    Atividade Index

---
+++ Atividade index.R
@@ -0,0 +1,105 @@
+times_futebol<-c("sport","nautico","santa cruz","corinthians","flamengo")
+
+install.packages("eeptools")
+
+library("eeptools")
+
+timesFutebol<-c("sport","nautico","santa cruz","corinthians","flamengo")
+
+fundaçãoTimes <-c("1905","1901","1914","1910","1895")
+
+idadeTimes <-c ("117","121","108","112","127")
+
+titulosTimes <-c ("42","21","29","28","35")
+
+listaTimes <-c (data.frame(
+  fundação = fundaçãoTimes,
+  idade=idadeTimes,
+  titulos=titulosTimes
+))
+listaTimes
+str(listaTimes)
+
+legiao_urbana <-c("Renato Russo", "Dado VilaLobos", "Marcelo Bonfá")
+
+play_list <- list("A=(legião_urbana", "titãs", "capital_inicial") B=("skank", "kid_abelha", "ira") C=("raimundo","RPM", "jota_quest"))
+
+play_list <- list(A="internacionais", B=c("pop_rock"), C=c('forro', 'sertanejo'))
+
+View(play_list)
+
+summary(play_list)
+
+length(play_list)
+
+str(play_list)
+
+a <- sum(1,3,5)
+
+b <- sum(0,2,7)
+
+c <- sum(1,2, 4)
+
+if (a < b)
+
+
+if (a < b) {
+  
+  return (b)
+
+
+idadeTimes <-c ("117","121","108","112","127")
+
+titulosTimes <-c ("42","21","29","28","35")
+
+summary(idadeTimes)
+
+str(times_futebol)
+
+lm(idadeTimes~titulosTimes)
+
+summary(idadeTimes~titulosTimes)
+
+sport <- 42
+
+nautico <- 21
+
+santa <- 29
+
+
+
+regTimes <- lm(idadeTimes~.,titulosTimes)
+
+
+d <- c("3", "4","5")
+
+
+d <- c("9","10")
+
+idadeTimes <-c ("117","121","108","112","127")
+
+titulosTimes <-c ("42","21","29","28","35")
+
+TimesFutebol<-c("sport","nautico","santa cruz","corinthians","flamengo")
+
+regTimes <- lm(idadeTimes~titulosTimes)
+
+str(TimesFutebol)
+
+summary(TimesFutebol)
+
+
+timesFutebol<-c("sport","nautico","santa cruz","corinthians","flamengo")
+
+fundaçãoTimes <-c("1905","1901","1914","1910","1895")
+
+idadeTimes <-c ("117","121","108","112","127")
+
+titulosTimes <-c ("42","21","29","28","35")
+
+listaTimes <-c (data.frame(
+  fundação = fundaçãoTimes,
+  idade=idadeTimes,
+  titulos=titulosTimes
+))
+listaTimes
