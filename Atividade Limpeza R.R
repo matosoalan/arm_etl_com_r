083f347b95740f592a658e7e139bb6e6f4a7efe3
matosoalan <110362847+matosoalan@users.noreply.github.com>
2022-09-13 15:30:07 -0300

    Atividade Limpeza R

---
+++ Atividade Limpeza R.R
@@ -0,0 +1,37 @@
+library(datasets)
+library(dplyr)
+library(tidyr)
+library(tidyverse)
+library(data.table)
+
+install.packages("tidyverse")
+
+library(data.table)
+
+install.packages("funModeling")
+
+library(tidyverse)
+
+library(funModeling)
+
+general_data <- fread("https://covid.ourworldindata.org/data/owid-covid-data.csv") # carrega dados de covid19 no mundo
+
+latin_america_countries <-c("Argentina", "Brazil", "Bolivia", "Chile", "Colombia", "Costa Rica", "Cuba", "Dominican Republic", "Ecuador", "El Salvador", "Guatemala", "Haiti", "Honduras", "Mexico", "Nicaragua", "Panama", "Paraguay", "Peru", "Uruguay", "Venezuela") # vetor que identifica países latino americanos
+
+latin_america <- general_data %>% filter(location %in% latin_america_countries) # filtra casos apenas no vetor
+
+latin_america <- latin_america %>% select(location, new_cases, new_deaths)
+
+status(latin_america) # estrutura dos dados (missing etc)
+freq(latin_america) # frequência das variáveis fator
+plot_num(latin_america) # exploração das variáveis numéricas
+profiling_num(latin_america) # estatísticas das variáveis numéricas
+
+latin_america %>% filter(new_cases < 0)
+
+latin_america <- latin_america %>% filter(new_cases>=0)
+latin_america_na <- na.omit(latin_america)
+
+View(latin_america)
+
+View(latin_america_na)
